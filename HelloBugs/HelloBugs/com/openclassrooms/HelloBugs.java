38f053f80e90a9fd13658f6f6e7838c65e2b54a7
AZOrca <zrrzheng@gmail.com>
2020-02-06 11:53:46 -0500

    Applied code formating

--- HelloBugs/HelloBugs/com/openclassrooms/HelloBugs.java
+++ HelloBugs/HelloBugs/com/openclassrooms/HelloBugs.java
@@ -3,11 +3,11 @@ package com.openclassrooms;
 public class HelloBugs {
 
 	public static void main(String[] args) {
-		
-		for(int i=0;i<5;i++) {
-		System.out.println("Hello buggy code!");
+
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Hello buggy code!");
 		}
-				
+
 	}
 
 }
